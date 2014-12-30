948a726b3ea160b16e5fc7062be363f826cd6e97
alex lines <alex.lines@gmail.com>
2014-12-30 16:39:40 -0500

    cleanup + docs
     * basic readme file
     * clean up python code, but it's still a mess

--- generate_json.py
+++ generate_json.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 
 import csv
+import sys
 from jinja2 import Environment, PackageLoader
 
-file = 'eat-notation-office.csv'
+def get_template(default='map-template.json'):
+    env = Environment(loader=PackageLoader('app', 'templates'))
+    template = env.get_template(default)
+    return template
 
-env = Environment(loader=PackageLoader('app', 'templates'))
-template = env.get_template('map-template.json')
+def get_data(file):
+    reader = csv.DictReader(open(file), delimiter='|')
+    return [ row for row in reader ]
 
-reader = csv.DictReader(open(file), delimiter='|')
+def workflow(input_file):
+    template = get_template()
+    data = get_data(input_file)
+    print template.render(restaurants=data)
+    # should probably validate the generated json
 
-result = {}
-restaurants  = []
-for row in reader:
-    restaurants.append(row)
- 
-print template.render(restaurants=restaurants)
+if __name__ == '__main__':
+    input_file = 'eat-notation-office.csv'
+    sys.exit( workflow(input_file) )
---
+++ readme.md
@@ -0,0 +1,2 @@
+
+to add restaurants, don't edit the json directly, add their info to the csv file and re-run generate_json.py
